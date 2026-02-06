0140a9bbfb684b8bc142cae72cbeeb95821e8757
Shadhir Pasha <shadhirpashamohammed@gmail.com>
2026-02-06 23:31:12 +0530

    Add files via upload

--- patterns.java
+++ patterns.java
@@ -1,13 +1,21 @@
 public class patterns {
     public static void main(String[]args){
-        // for(int i =0;i<=4;i++){
-            // System.out.println("****");
+
+        // rectangular pattern
+
+        // nrml
+
+
+        //  for(int i =0;i<=4;i++){
+        //     System.out.println("****");
         // }
+
+
         // int n=5;
         // int m=5;
 
-        // nested loop
-        // outer loop
+        // // nested loop
+        // // outer loop
         // for(int i=1;i<=n;i++){
         //     // inner loop
         //     for(int j=1;j<=m;j++){
@@ -16,24 +24,73 @@ public class patterns {
         //     System.out.println(" ");
         // }
 
+
         // hollow rectangle
-        
-        int n=4;
-        int m=5;
 
-        // outr loop
-        for(int i=1;i<=n;i++){
-            // inner loop
-            for( int j=1;j<=m;j++){
-                if(i==1 || j==1 || i==n || j==m){
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+        
+    //     int n=4;
+    //     int m=5;
+
+    //     // outr loop
+    //     for(int i=1;i<=n;i++){
+    //         // inner loop
+    //         for( int j=1;j<=m;j++){
+    //             if(i==1 || j==1 || i==n || j==m){
+    //                 System.out.print("*");
+    //             }else{
+    //                 System.out.print(" ");
+    //             }
+    //         }
+    //         System.out.println();
         
 
-    } 
+    // } 
+
+
+
+    // half pyramid
+
+
+    // System.out.println("*\n**\n***\n****");
+
+    // loops
+    
+    // int n=4;
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+
+    // inverted half pyramid
+
+
+//     int n=4;
+//     for(int i=4;i>=1;i--){
+//         for(int j=1;j<=i;j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+
+    //  inverted half pyramid
+    // (rotated by 180)
+
+
+    // int n=4;
+    // for(int i=1;i<=n; i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
 }
 }
+
+ 
