afed00658d1edb5cae85085a9161a81325d07f2f
Shadhir Pasha <shadhirpashamohammed@gmail.com>
2026-02-09 20:53:45 +0530

    Add files via upload

--- patterns.java
+++ patterns.java
@@ -90,6 +90,16 @@ public class patterns {
     //     System.out.println();
     // }
 
+    // half pyramid with numbers
+
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+
 }
 }
 
