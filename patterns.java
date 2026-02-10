11f2e3d763ee2c1ca1d655a35caff734446e645b
Shadhir Pasha <shadhirpashamohammed@gmail.com>
2026-02-10 23:21:54 +0530

    Add files via upload

--- patterns.java
+++ patterns.java
@@ -92,13 +92,58 @@ public class patterns {
 
     // half pyramid with numbers
 
+    // int n=5;
+    // // outer loop
+    // for(int i=1;i<=n;i++){
+    //     // inner loop
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print(j);
+    //     }
+    //     System.out.println();
+    // }
+
+    // inverted half pyramid with numbers
+
+    // outer loop
+    // int n=5;
+    // for(int i=1;i<=n;i++){
+    //     // inner loop
+    //     for(int j=1;j<=n-i+1;j++){
+    //         System.out.print(j);
+    //     }
+    //     System.out.println();
+    // }
+
+    // floyd's Triangle
+
+    // int n=5;
+    // int number = 1;
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print(number+" ");
+    //         // number =no.+1
+    //         number++;
+    //     }
+    //     System.out.println();
+    // }
+
+    // 0-1 triangle
     int n=5;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
-            System.out.print(j);
+            int sum =i+j;
+            if(sum %2 ==0){
+                // even
+                System.out.print("1 ");
+            }else{
+                // odd
+                System.out.print("0 ");
+            }
+            
         }
         System.out.println();
     }
+
 
 }
 }
