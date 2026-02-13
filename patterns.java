57be9fd9c71bf90eef6511a487b1462e8db9d8ee
Shadhir Pasha <shadhirpashamohammed@gmail.com>
2026-02-13 23:13:52 +0530

    Add files via upload

--- patterns.java
+++ patterns.java
@@ -166,21 +166,43 @@ public class patterns {
         // inverted full  pyramid
         
         
-        int n = 5;  // number of rows
+        // int n = 5;  // number of rows
 
-        for (int i = n; i >= 1; i--) {
+        // for (int i = n; i >= 1; i--) {
 
-            // print spaces
-            for (int j = 0; j < n - i; j++) {
+        //     // print spaces
+        //     for (int j = 0; j < n - i; j++) {
+        //         System.out.print(" ");
+        //     }
+
+        //     // print stars
+        //     for (int j = 1; j <= (2 * i - 1); j++) {
+        //         System.out.print("*");
+        //     }
+
+        //     // move to next line
+        //     System.out.println();
+        // }
+
+        // diamond pattern
+
+        int n = 5; 
+
+        // Upper half
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++)
                 System.out.print(" ");
-            }
+            for (int j = 0; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
 
-            // print stars
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                System.out.print("*");
-            }
-
-            // move to next line
+        // Lower half
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++)
+                System.out.print(" ");
+            for (int j = 0; j <= i; j++)
+                System.out.print("* ");
             System.out.println();
         }
 
